48a5be136dac5e612ff0a8dc56a85236cff7ec24
Don Hamilton III <donhamiltoniii@gmail.com>
2017-11-13 23:09:44 -0500

    Finish example

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Use jQuery to complete the following challenges
+
+// Get the value from the text input element and populate the output with the input value
+// Style the 'h1' element dynamically when the user hovers over it
+// When a user clicks the 'hide this card' link in the cards, hide that card.
+// Change the style of the 'p' elements when you double click them
+// Add an element to the 'main' element when the user adds html in an alert box.
