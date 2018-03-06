fadf7bfe6d36f52e284f4e44093816c753c35476
Don Hamilton III <donhamiltoniii@gmail.com>
2018-03-06 11:53:38 -0500

    Update HTML to buttons instead of anchor tags

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 // Use jQuery to complete the following challenges
 
 // Get the value from the text input element and populate the output with the input value
+
 // Style the 'h1' element dynamically when the user hovers over it
 // When a user clicks the 'hide this card' link in the cards, hide that card.
+$('.card button').click(function() {
+	$(this)
+		.parent()
+		.hide(1000)
+})
 // Change the style of the 'p' elements when you double click them
 // Add an element to the 'main' element when the user adds html in an alert box.
